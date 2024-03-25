a39489a2b54ec38b720d957cf513e3409c5942da
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-03-25 19:17:04 +0000

    feat: basic page content

--- src/index.js
+++ src/index.js
@@ -11,6 +11,26 @@ import "./styles.css";
     const menuBtn = document.querySelector("#Menu");
     const aboutBtn = document.querySelector("#About");
 
+    const mainMenu = () => {
+        const heading = document.createElement("h1");
+        heading.textContent = "Bob's Burgers";
+
+        content.appendChild(heading);
+
+        const location = document.createElement("h3");
+        location.textContent = "Conveniently located on Ocean Avenue, Seymour's Bay, New Jersey";
+
+        content.appendChild(location);
+
+        const quote = document.createElement("p");
+        quote.textContent = "Are you tired of restaurants saying, 'We have the best burgers in town'? Well, you're in luck, because I'm here to tell you that WE have the best burgers in town. Argument settled.";
+
+        content.appendChild(quote);
+    }
+
+    homeBtn.addEventListener("click", mainMenu);
+
+    mainMenu();
     // TODO Attach Functions to each button
     // TODO Clear any existing content
     // TODO content files
